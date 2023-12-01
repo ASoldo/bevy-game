4908763ad208450480e78013cd4246a0bc15b6e6
ASoldo <soldo.andrej@gmail.com>
2023-12-01 20:12:10 +0100

    Fix components for inspector feature

--- src/components/component.rs
+++ src/components/component.rs
@@ -1,17 +1,5 @@
 use bevy::prelude::*;
-#[cfg(feature = "inspector")]
-use bevy_inspector_egui::prelude::ReflectInspectorOptions;
-#[cfg(feature = "inspector")]
-use bevy_inspector_egui::InspectorOptions;
 
-#[cfg(feature = "inspector")]
-#[derive(Component, Reflect, Resource, Default, InspectorOptions)]
-#[reflect(Resource, InspectorOptions)]
-pub struct MarkerComponent {
-    pub marker: String,
-}
-
-#[cfg(not(feature = "inspector"))]
 #[derive(Component, Reflect, Resource, Default)]
 pub struct MarkerComponent {
     pub marker: String,
